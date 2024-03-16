1be82a53ff941b46e1de721c76bb86a79a0e116b
SangJin Lee <9607sj@gmail.com>
2024-03-16 14:31:09 +0900

    스토어 및 아이콘기능 보완 3월16일

--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useCallback } from 'react';
+import axios from 'axios'; // 서버 요청을 위해 axios를 임포트합니다.
 
 const UserContext = createContext();
 
@@ -7,8 +8,22 @@ export const useUser = () => useContext(UserContext);
 export const UserProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
 
+  // 사용자 정보를 새로고침하는 함수입니다.
+  const refreshUserData = useCallback(async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/userData', { withCredentials: true });
+      if (response.data && response.data.userData) {
+        setUserData(response.data.userData);
+      } else {
+        console.log('아이콘 구매 성공후 유저데이터 새로고침.');
+      }
+    } catch (error) {
+      console.error('사용자 정보 새로고침 중 오류가 발생했습니다:', error);
+    }
+  }, []);
+
   return (
-    <UserContext.Provider value={{ userData, setUserData }}>
+    <UserContext.Provider value={{ userData, setUserData, refreshUserData }}>
       {children}
     </UserContext.Provider>
   );
